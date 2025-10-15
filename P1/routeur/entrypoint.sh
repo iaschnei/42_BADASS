faefa5c221a3953c003907f5a489ea2542b0ee27
iaschnei <iaschnei@student.42.fr>
2025-10-15 12:46:12 +0200

    switched to pre made image

--- P1/routeur/entrypoint.sh
+++ P1/routeur/entrypoint.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# Start zebra (-d = daemon instead of foreground, -F traditional = type of naming conventions to use, -A = bind to an address, -s 90000 = max number of fd zebra can use) Note : all of these options are shown in the subject's expected result
+
+# Start the daemons in the background
 /usr/lib/frr/zebra -d -F traditional -A 127.0.0.1 -s 90000
-
-# Start BGP, OSPF and IS-IS in the same way
 /usr/lib/frr/bgpd -d -F traditional -A 127.0.0.1
-
 /usr/lib/frr/ospfd -d -F traditional -A 127.0.0.1
-
 /usr/lib/frr/isisd -d -F traditional -A 127.0.0.1
 
-# Default interactive console for GNS3 to use with frr suite
-exec vtysh
+
+# Wait a bit for all daemons to start
+sleep 2
+
+# Start a shell so we can use 'ps' in GNS3
+exec sh
