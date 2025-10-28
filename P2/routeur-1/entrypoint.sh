4322de153f43c3813037c321dbf6b8fb04a91e8f
iaschnei <iaschnei@student.42.fr>
2025-10-28 11:53:27 +0100

    Done with Part 2

--- P2/routeur-1/entrypoint.sh
+++ P2/routeur-1/entrypoint.sh
@@ -24,13 +24,14 @@ ip addr add 10.1.1.1/24 dev eth0
 ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
 
 ip addr add 20.1.1.1/24 dev vxlan10
+ip link set dev vxlan10 up
+
 
 # Link the bridge to the router's eth1 interface and to vxlan10
 brctl addif br0 eth1
 brctl addif br0 vxlan10
 
 
-ip link set device vxlan10 up
 
 # To summarise : 
 # This router is part of a VXLAN. The hosts will connect on eth1 through a bridge from eth1 to vxlan10
