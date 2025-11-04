d7f0e8bd1308c031f8d8147d470bd271d5d6d81c
iaschnei <iaschnei@student.42.fr>
2025-11-04 13:56:16 +0100

    removed bridge config in leaf 3

--- P3/leaf_3/entrypoint.sh
+++ P3/leaf_3/entrypoint.sh
@@ -7,17 +7,5 @@
 # Wait a bit for all daemons to start
 sleep 2
 
-
-# Config the router, the bridge and the VXLAN
-# (dev == device)
-ip link add br0 type bridge
-ip link set dev br0 up
-
-ip link add name vxlan10 type vxlan id 10 dstport 4789
-
-ip link set dev vxlan10 up
-brctl addif br0 eth0
-brctl addif br0 vxlan10
-
 # Start a shell
 exec sh
